9ad9ff0d12969a897db80f835e75b4f6ef080792
Margarita <nguyenmargarita.w@gmail.com>
2023-07-13 15:20:24 +0300

    export techcard

--- Agro/Controllers/TechCardReportController.cs
+++ Agro/Controllers/TechCardReportController.cs
@@ -1,12 +1,10 @@
 ﻿using Agro.DataAccess.Entities;
 using Agro.Models;
-using Agro.Pagination;
 using Agro.Services.Interfaces;
 using AutoMapper;
+using ClosedXML.Excel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Text;
-using System.Web;
 
 namespace Agro.Controllers
 {
@@ -28,6 +26,44 @@ namespace Agro.Controllers
             var techCardReports = await _techCardReportService.GetAllTechCardReports(dosingTaskId, batchNum);
             var model = _mapper.Map<IEnumerable<TechCardReport>, IList<TechCardReportViewModel>>(techCardReports);
             return View(model);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public FileResult Export(IList<TechCardReportViewModel> model)
+        {
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                var workSheet = wb.Worksheets.Add();
+                workSheet.Cell(0, 0).Value = "Технологическая карта";
+                workSheet.Cell(1, 0).Value = $"Номер заказа: {model.Max(x => x.ManufNr)}";
+                workSheet.Cell(2, 0).Value = $"Продукт: {model.Max(x => x.ProductName)}";
+
+                workSheet.Cell(4, 0).Value = "№";
+                workSheet.Cell(4, 1).Value = "Сырье";
+                workSheet.Cell(4, 2).Value = "Заданный вес, кг";
+                workSheet.Cell(4, 3).Value = "Отдозированный вес, кг";
+                workSheet.Cell(4, 4).Value = "Отклонение, кг";
+                workSheet.Cell(4, 5).Value = "Начало дозирования";
+                workSheet.Cell(4, 6).Value = "Конец дозирования";
+
+                for (int i = 0; i < model.Count; i++)
+                {
+                    workSheet.Cell(i + 5, 0).Value = $"{model[i].Num}";
+                    workSheet.Cell(i + 5, 1).Value = $"{model[i].ResourceName}";
+                    workSheet.Cell(i + 5, 2).Value = $"{model[i].ReqWeight.ToString("F2")}";
+                    workSheet.Cell(i + 5, 3).Value = $"{model[i].RealWeight.ToString("F2")}";
+                    workSheet.Cell(i + 5, 4).Value = $"{(model[i].RealWeight - model[i].ReqWeight).ToString("F2")}";
+                    workSheet.Cell(i + 5, 5).Value = $"{model[i].StartTime}";
+                    workSheet.Cell(i + 5, 6).Value = $"{model[i].EndTime}";
+                }
+
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    wb.SaveAs(stream);
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "TechCard.xlsx");
+                }
+            }
         }
     }
 }
